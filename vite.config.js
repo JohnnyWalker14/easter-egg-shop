01c83879b4d25d3505c8576a1805be55c281be12
JohnnyWalker14 <135173548+JohnnyWalker14@users.noreply.github.com>
2026-03-25 20:26:19 +0200

    Modify Vite config for base URL and server settings
    Updated base URL for deployment and configured server settings.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Αλλάζουμε το './' με το όνομα του repo σου ανάμεσα σε slashes
+  base: '/easter-egg-shop/', 
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    allowedHosts: true
+  }
+})import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
   base: './',
   plugins: [react()],
   server: {
